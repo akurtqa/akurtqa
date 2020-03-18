b7494341ee0e33ee0eed2d4a05e1a352b3160fdf
akurtqa <akurtqa@gmail.com>
2020-03-17 20:56:35 -0400

    mars project created

---
+++ src/mars/test.java
@@ -0,0 +1,10 @@
+package mars;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/test/test2.java
@@ -0,0 +1,10 @@
+package test;
+
+public class test2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
